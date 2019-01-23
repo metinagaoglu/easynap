fa6a4f3a39b910635b7eb9afaa92b178f27c3a83
metin <metnagaoglu@gmail.com>
2019-01-23 22:05:01 +0300

    dovend and mongoose added.Mongodb connected

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 const { ApolloServer } = require('apollo-server-express');
 const { importSchema } = require('graphql-import');
+const mongoose = require('mongoose');
 
 const resolvers = require('./graphql/resolvers/index');
 
@@ -8,6 +10,10 @@ const server = new ApolloServer({
     typeDefs: importSchema('./graphql/types/schema.graphql'),
     resolvers
 });
+
+mongoose.connect(process.env.DB_URI,{ useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(e => console.log(e));
 
 const app = express();
 server.applyMiddleware({ app });
